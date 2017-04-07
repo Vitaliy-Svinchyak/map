d86115d517f15c44157fda9de817033046663952
Vitaliy-Svinchyak <vitalii.svinchyak@gmail.com>
2017-04-08 01:27:38 +0300

    index.js

---
+++ map.component/index.js
@@ -0,0 +1,25 @@
+var canvas = document.getElementById('canvas');
+var map = new Map(canvas);
+map.renderMapAreas([
+    new Area([
+        new Point(200, 436),
+        new Point(224, 420),
+        new Point(261, 441),
+        new Point(269, 477),
+        new Point(220, 464),
+    ], ''),
+    new Area([
+        new Point(220, 464),
+        new Point(269, 477),
+        new Point(264, 523),
+        new Point(245, 545),
+        new Point(213, 521),
+    ], ''),
+    new Area([
+        new Point(269, 477),
+        new Point(316, 474),
+        new Point(392, 491),
+        new Point(325, 547),
+        new Point(264, 523),
+    ], '')
+]);
